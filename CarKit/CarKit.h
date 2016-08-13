0751776c752d50025593bf59295109fdef30f3a1
Michael Henry <mike@digitaljavelina.com>
2016-08-12 19:42:52 -0700

    initial commit

---
+++ CarKit/CarKit.h
@@ -0,0 +1,19 @@
+//
+//  CarKit.h
+//  CarKit
+//
+//  Created by Michael Henry on 9/9/15.
+//  Copyright (c) 2015 Digital Javelina, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarKit.
+FOUNDATION_EXPORT double CarKitVersionNumber;
+
+//! Project version string for CarKit.
+FOUNDATION_EXPORT const unsigned char CarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarKit/PublicHeader.h>
+
+
